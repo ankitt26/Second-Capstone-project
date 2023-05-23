e765a39bec6156f56e179fd46bf2efac74a3c571
Ankit <ankit26kushwa@gmail.comm>
2023-05-23 23:51:12 +0530

    fetch Api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
-import './style.css';
+import displayCard from '../modules/displayCard';
+import "./style.css";
 
-// const API = 'https://api.tvmaze.com/shows';
+const API = "https://api.tvmaze.com/shows";
+const fetch_api = async () => {
+  await fetch(API)
+    .then((response) => response.json())
+    .then((json) => json)
+    .then((data) => {
+      displayCard(data);
+    });
+};
+
+fetch_api();
+
