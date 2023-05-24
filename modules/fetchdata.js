41ac1c0b6d12604986131505ab8e44f53a1a4d5e
Ankit <ankit26kushwa@gmail.comm>
2023-05-24 20:36:57 +0530

    Add module file

---
+++ modules/fetchdata.js
@@ -0,0 +1,13 @@
+import displayCard from './displayCard';
+
+const API = 'https://api.tvmaze.com/shows';
+
+export default async () => {
+  await fetch(API)
+    .then((response) => response.json())
+    .then((json) => json)
+    .then((data) => {
+      displayCard(data);
+    });
+};
+
--- src/index.js
+++ src/index.js
@@ -1,14 +1,4 @@
-import displayCard from '../modules/displayCard.js';
+import fetchApi from '../modules/fetchdata';
 import './style.css';
-
-const API = 'https://api.tvmaze.com/shows';
-const fetchApi = async () => {
-  await fetch(API)
-    .then((response) => response.json())
-    .then((json) => json)
-    .then((data) => {
-      displayCard(data);
-    });
-};
 
 fetchApi();
